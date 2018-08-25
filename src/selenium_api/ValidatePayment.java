f782ce012e755c8c7aec223f96079a56b0995a74
Phuong Tran <bichphuong1209@gmail.com>
2018-08-25 09:44:26 +0700

    update testscript

--- src/selenium_api/ValidatePayment.java
+++ src/selenium_api/ValidatePayment.java
@@ -16,22 +16,58 @@ import org.testng.annotations.Test;
 public class ValidatePayment {
 	WebDriver driver;
 
-	private void commonTestProcedure(String LastName_payment, String FirstName_payment, String Gender_payment,
-			String Baggage_payment, String GenderContact_pay, String LastNameContact_pay, String FirstNameContact_pay,
-			String email_contact, String phone_contact) throws InterruptedException {
+	private void commonTestProcedure(String lastname_adult, String firstname_adult, String gender_adult,
+			String firstname_child, String lastname_child, String gender_child, String day_child, String month_child,
+			String year_child, String lastname_infant, String firstname_infant, String gender_infant, String day_infant,
+			String month_infant, String year_infant, String gender_contact, String lastName_contact,
+			String firstname_contact, String email_contact, String phone_contact) throws InterruptedException {
 
-		driver.findElement(By.xpath("//input[@ng-model='adult.lastName']")).sendKeys(LastName_payment);
+		// NHAP THONG TIN NGUOI LON
+		driver.findElement(By.xpath("//input[@ng-model='adult.lastName']")).sendKeys(lastname_adult);
 		// Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[contains(@class,'first-name')]")).sendKeys(FirstName_payment);
+		driver.findElement(By.xpath("//input[contains(@class,'first-name')]")).sendKeys(firstname_adult);
 		new Select(driver.findElement(By.xpath("(//select[@ng-model='adult.gender'])[1]")))
-				.selectByVisibleText(Gender_payment);
+				.selectByVisibleText(gender_adult);
+
+		// NHAP THONG TIN TRE EM 
+		driver.findElement(By.xpath("//input[@ng-model='child.lastName']")).sendKeys(firstname_child);
+		driver.findElement(By.xpath("//input[@ng-model='child.firstName']")).sendKeys(lastname_child);
+		new Select(driver.findElement(By.xpath("(//select[@ng-model='child.gender'])[1]")))
+				.selectByVisibleText(gender_child);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='child.dayOfBirth']")))
+				.selectByVisibleText(day_child);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='child.monthOfBirth']")))
+				.selectByVisibleText(month_child);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='child.yearOfBirth']")))
+				.selectByVisibleText(year_child);
+		Thread.sleep(2000);
+
+		// NHAP THONG TIN EM BE
+		driver.findElement(By.xpath("//input[@ng-model='infant.lastName']")).sendKeys(lastname_infant);
+		driver.findElement(By.xpath("//input[@ng-model='infant.firstName']")).sendKeys(firstname_infant);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='infant.gender'][1]")))
+				.selectByVisibleText(gender_infant);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='infant.dayOfBirth']")))
+				.selectByVisibleText(day_infant);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='infant.monthOfBirth']")))
+				.selectByVisibleText(month_infant);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath("//select[@ng-model='infant.yearOfBirth']")))
+				.selectByVisibleText(year_infant);
+		Thread.sleep(2016);
 
 		// NHẬP THÔNG TIN LIÊN HỆ
-		new Select(driver.findElement(By.id("ticket-booking-select-title"))).selectByVisibleText(GenderContact_pay);
-		driver.findElement(By.xpath("//input[@ng-model='contactInfo.lastName']")).sendKeys(LastNameContact_pay);
-		driver.findElement(By.xpath("//input[@ng-model='contactInfo.firstName']")).sendKeys(FirstNameContact_pay);
+		new Select(driver.findElement(By.id("ticket-booking-select-title"))).selectByVisibleText(gender_contact);
+		driver.findElement(By.xpath("//input[@ng-model='contactInfo.lastName']")).sendKeys(lastName_contact);
+		driver.findElement(By.xpath("//input[@ng-model='contactInfo.firstName']")).sendKeys(firstname_contact);
 		driver.findElement(By.xpath("//input[@ng-model='contactInfo.email']")).sendKeys(email_contact);
 		driver.findElement(By.xpath("//input[@ng-model='contactInfo.phone1']")).sendKeys(phone_contact);
+
 	}
 
 	@Test(priority = 0)
@@ -50,36 +86,43 @@ public class ValidatePayment {
 	}
 
 	@Test(priority = 1)
-	@Parameters({ "LastName_payment", "FirstName_payment", "Gender_payment", "Baggage_payment", "GenderContact_pay",
-			"LastNameContact_pay", "FirstNameContact_pay", "email_contact", "phone_contact" })
-	public void TC_WithoutSelectPaymentMethod(String LastName_payment, String FirstName_payment, String Gender_payment,
-			String Baggage_payment, String GenderContact_pay, String LastNameContact_pay, String FirstNameContact_pay,
-			String email_contact, String phone_contact) throws InterruptedException {
-
-		commonTestProcedure(LastName_payment, FirstName_payment, Gender_payment, Baggage_payment, GenderContact_pay,
-				LastNameContact_pay, FirstNameContact_pay, email_contact, phone_contact);
-
-		WebElement paymentbtn = driver.findElement(By.cssSelector(".flight-payment-button"));
-		paymentbtn.click();
-		String errormssage = driver.findElement(By.cssSelector(".message")).getText();
-		Assert.assertEquals("Vui lòng lựa chọn phương thức thanh toán", errormssage);
-		System.out.println(errormssage);
-
-	}
-
-	// Validate vouchercode
-	@Test(priority = 2)
 	@Parameters({ "vouchercode" })
 	public void TC_TC_ValidateVoucherCode(String vouchercode) throws InterruptedException {
-
+		driver.navigate().refresh();
 		WebElement voucher = driver.findElement(By.cssSelector(".coupon-code"));
 		voucher.sendKeys(vouchercode);
 		WebElement voucherbtn = driver.findElement(By.cssSelector(".btn-submit-code"));
 		voucherbtn.click();
+		Thread.sleep(3000);
 		String errormssage = driver.findElement(By.xpath("//span[contains(text(),'Mã khuyến mãi không hợp lệ!')]"))
 				.getText();
-		Assert.assertEquals("Mã khuyến mãi không hợp lệ!", errormssage);
-		System.out.println(errormssage);
+		System.out.println("TC_TC_ValidateVoucherCode errormssage: " + errormssage);
+		Assert.assertEquals(errormssage, "Mã khuyến mãi không hợp lệ!");
+	}
+
+	@Test(priority = 2)
+	@Parameters({ "lastname_adult", "firstname_adult", "gender_adult", "firstname_child", "lastname_child","gender_child", "day_child",
+			"month_child", "year_child", "lastname_infant", "firstname_infant", "gender_infant", "day_infant",
+			"month_infant", "year_infant", "gender_contact", "lastname_contact", "firstname_contact", "email_contact",
+			"phone_contact" })
+	public void TC_WithoutSelectPaymentMethod(String lastname_adult, String firstname_adult, String gender_adult,
+			String firstname_child, String lastname_child, String gender_child, String day_child, String month_child,
+			String year_child, String lastname_infant, String firstname_infant, String gender_infant, String day_infant,
+			String month_infant, String year_infant, String gender_contact, String lastname_contact,
+			String firstname_contact, String email_contact, String phone_contact) throws InterruptedException {
+		driver.navigate().refresh();
+		commonTestProcedure(lastname_adult, firstname_adult, gender_adult, firstname_child, lastname_child,
+				gender_child, day_child, month_child, year_child, lastname_infant, firstname_infant, gender_infant,
+				day_infant, month_infant, year_infant, gender_contact, lastname_contact, firstname_contact,
+				email_contact, phone_contact);
+
+		driver.navigate().refresh();
+		WebElement paymentbtn = driver.findElement(By.cssSelector(".flight-payment-button"));
+		paymentbtn.click();
+		Thread.sleep(8000);
+		String errormssage = driver.findElement(By.cssSelector(".message")).getText();
+		System.out.println("WithoutSelectPaymentMethod: " + errormssage);
+		Assert.assertEquals(errormssage, "Vui lòng lựa chọn phương thức thanh toán");
 
 	}
 
@@ -87,7 +130,7 @@ public class ValidatePayment {
 	public void beforeClass() {
 		driver = new FirefoxDriver();
 		driver.get(
-				"https://www.tripi.vn/flights/booking?requestIdOutBound=-613056168&fromAirport=HAN&toAirport=SGN&departureDate=27-08-2018&ticketClassId=1&numAdults=1&numChildren=0&numInfants=0&oneWay=1&ticketOutBoundId=-971122508&agencyIdOutBound=3");
+				"https://www.tripi.vn/flights/booking?requestIdOutBound=-980413262&fromAirport=HAN&toAirport=SGN&departureDate=30-08-2018&ticketClassId=1&numAdults=1&numChildren=1&numInfants=1&oneWay=1&ticketOutBoundId=-1552192560&agencyIdOutBound=3");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
